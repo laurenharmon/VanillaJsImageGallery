4c61aa4b17640be18530e1675e56205d9f7fcbaf
Lauren Harmon <harmon.lauren@hotmail.com>
2021-07-21 12:45:18 -0500

    .

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const gallery = document.getElementById("gallery");
 const popup = document.getElementById("popup");
 const selectedImage = document.getElementById("selectedImage");
 
-for (let i = 1; i < 15; i++) {
+for (let i = 1; i <= 15; i++) {
     const image = document.createElement("img");
     image.src = `/images/citrus-${i}.jpg`;
     image.alt = `Citrus Picture Number ${i}`;
     image.classList.add("galleryImage");
     image.addEventListener("click", () => {
-        //popup
+        popup.style.transform = `translateY(0)`;
+        selectedImage.src = `/images/citrus-${i}.jpg`;
+        selectedImage.alt = `Citrus Picture Number ${i}`;
     })
     gallery.appendChild(image);
 };
+
+popup.addEventListener("click", () => {
+    popup.style.transform = `translateY(-100%)`;
+    popup.src = "";
+    popup.alt = "";
+})
